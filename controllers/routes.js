f8db4942564df64498832d3755b51b646444749a
Josh Lehne <jdlehne@gmail.com>
2017-10-26 15:14:37 -0500

    route work

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,44 +1,8 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var methodOverride = require('method-override');
-
-const PORT = process.env.PORT || 5050;
-let app = express();
-//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-//app.set('view engine', 'handlebars');
-
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(process.cwd() + '/public'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-app.use(methodOverride('_method'));
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-//require('./controllers/routes.js');
-
-mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/news-scrape", {
-  useMongoClient: true
-});
-
-
 // Routes
 
+var db = require("../models");
+
+module.exports = function(app) {
 // A GET route for scraping
 app.get("/scrape", function(req, res) {
   axios.get("http://www.gamespot.com/").then(function(response) {
@@ -147,25 +111,5 @@ app.post("/delete/:id", function(req, res) {
 		}
 	});
 });
-//===================
-/*
-//const db = mongoose.connection;
 
-// Show any mongoose errors
-db.on("error", function(error) {
-    console.log("Mongoose Error: ", error);
-});
-
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-    console.log("Mongoose connection successful.");
-    // start the server, listen on port 3000
-    app.listen(PORT, function() {
-        console.log("App running on port: " + PORT);
-    });
-});*/
-
-module.exports = app;
-
-app.listen(PORT);
-console.log("Listening on port: " + PORT);
+};
