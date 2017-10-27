796c7c420095f0769eb4fac07fe761cd5b1f00e5
Josh Lehne <jdlehne@gmail.com>
2017-10-26 23:13:20 -0500

    fixed controller routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,34 +1,42 @@
 // Routes
+const express = require('express'),
+  router = express.Router(),
+  request = require('request'),
+  cheerio = require('cheerio'),
+  db = require("../models"),
+  Article = require('../models/Article');
 
-var db = require("../models");
 
-module.exports = function(app) {
-// A GET route for scraping
-app.get("/scrape", function(req, res) {
-  axios.get("http://www.gamespot.com/").then(function(response) {
-    var $ = cheerio.load(response.data);
-    $("article.media").each(function(i, element) {
-      var result = {};
+  //==== Scrape route ===//
 
-      result.title = $(this)
-        .find("h3.media-title").text();
-      result.summary = $(this)
-        .find("p.media-deck").text();
-      result.link = "https://www.gamespot.com" + $(this)
-        .find("a").attr("href");
-      db.Article
-        .create(result)
-        .then(function(dbArticle) {
-          res.redirect("/");
-        })
-        .catch(function(err) {
-          res.json(err);
-        });
+  router.get('/scrape', function(req, res, next) {
+    request('https://www.gamespot.com', function(error, response, html) {
+      let $ = cheerio.load(html);
+      let results = [];
+      $('article.media').each(function(i, element) {
+        let title = $(this).find("h3.media-title").text(),
+          link = "https://www.gamespot.com" + $(this).find("a").attr("href"),
+          summary = $(this).find("p.media-deck").text();
+        result = {};
+        db.Article
+          .create(result)
+          .then(function(dbArticle) {
+            return res.redirect("/");
+          })
+          .catch(function(err) {
+            res.json(err);
+          });
+      });
+      next();
+
+    }, function(req, res) {
+      res.redirect('/');
     });
   });
-});
 
-app.get("/articles", function(req, res) {
+//====route to grab and display articles===//
+
+router.get("/articles", function(req, res) {
   db.Article
     .find({})
     .then(function(dbArticle) {
@@ -39,27 +47,31 @@ app.get("/articles", function(req, res) {
     });
 });
 
-//----get saved articles----/articles/saved will show as JSON--//
-app.get('/saved', function(req, res) {
-    db.Article
-        .find({})
-        .where('saved').equals(true)
-        .where('deleted').equals(false)
-        .populate('notes')
-        .exec(function(error, entries) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                res.status(200).json(entries);
-            }
-        });
+//===  get saved articles----/articles/saved will show as JSON  ===//
+
+router.get('/saved', function(req, res) {
+  db.Article
+    .find({})
+    .where('saved').equals(true)
+    .where('deleted').equals(false)
+    .populate('notes')
+    .exec(function(error, entries) {
+      if (error) {
+        console.log(error);
+        res.status(500);
+      } else {
+        res.status(200).json(entries);
+      }
+    });
 });
 
-//----get one article by id----//
-app.get("/articles/:id", function(req, res) {
+//===   get one article by id to grab notes ===//
+
+router.get("/articles/:id", function(req, res) {
   db.Article
-    .findOne({ _id: req.params.id })
+    .findOne({
+      _id: req.params.id
+    })
     .populate("note")
     .then(function(dbArticle) {
       res.json(dbArticle);
@@ -69,12 +81,19 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+//===  Route for saving/updating an Article's associated Note ===//
+
+router.post("/articles/:id", function(req, res) {
   db.Note
     .create(req.body)
     .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        note: dbNote._id
+      }, {
+        new: true
+      });
     })
     .then(function(dbArticle) {
       res.json(dbArticle);
@@ -84,32 +103,45 @@ app.post("/articles/:id", function(req, res) {
     });
 });
 
-//----working route to change saved db boolean to true---//
-app.post("/saved/:id", function(req, res) {
+//==== working route to change saved db boolean to true ===//
+
+router.post("/saved/:id", function(req, res) {
   console.log("save route hit");
-	db.Article.findOneAndUpdate({"_id": req.params.id}, {"saved": true}, {new:true})
-	.then(function(error, saved) {
-		if (error) {
-			console.log(error);
-		} else {
-			res.send(saved);
-		}
-	});
+  db.Article.findOneAndUpdate({
+      "_id": req.params.id
+    }, {
+      "saved": true
+    }, {
+      new: true
+    })
+    .then(function(error, saved) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.send(saved);
+      }
+    });
 });
 
+//==== Route to change saved to false and remove from saved articles ===//
 
-
-app.post("/delete/:id", function(req, res) {
+router.post("/delete/:id", function(req, res) {
   console.log("delete route hit");
-	db.Article.findOneAndUpdate({"_id": req.params.id}, {"saved":false}, {new:true})
-	.then(function(err, deleted) {
-    res.redirect("/saved");
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(deleted);
-		}
-	});
+  db.Article.findOneAndUpdate({
+      "_id": req.params.id
+    }, {
+      "saved": false
+    }, {
+      new: true
+    })
+    .then(function(err, deleted) {
+      res.redirect("/saved");
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(deleted);
+      }
+    });
 });
 
-};
+module.exports = router;
--- server.js
+++ server.js
@@ -9,144 +9,22 @@ mongoose.Promise = Promise;
 
 const PORT = process.env.PORT || 5000;
 let app = express();
-//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-//app.set('view engine', 'handlebars');
 
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
+ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(process.cwd() + '/public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride('_method'));
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-//require('./controllers/routes.js');
-
-// Routes
-
-// A GET route for scraping
-app.get("/scrape", function(req, res) {
-  axios.get("http://www.gamespot.com/").then(function(response) {
-    var $ = cheerio.load(response.data);
-    $("article.media").each(function(i, element) {
-      var result = {};
-
-      result.title = $(this)
-        .find("h3.media-title").text();
-      result.summary = $(this)
-        .find("p.media-deck").text();
-      result.link = "https://www.gamespot.com" + $(this)
-        .find("a").attr("href");
-      db.Article
-        .create(result)
-        .then(function(dbArticle) {
-          res.redirect("/");
-        })
-        .catch(function(err) {
-          res.json(err);
-        });
-    });
-  });
-});
-
-app.get("/articles", function(req, res) {
-  db.Article
-    .find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-//----get saved articles----/articles/saved will show as JSON--//
-app.get('/saved', function(req, res) {
-    db.Article
-        .find({})
-        .where('saved').equals(true)
-        .where('deleted').equals(false)
-        .populate('notes')
-        .exec(function(error, entries) {
-            if (error) {
-                console.log(error);
-                res.status(500);
-            } else {
-                res.status(200).json(entries);
-            }
-        });
-});
-
-//----get one article by id----//
-app.get("/articles/:id", function(req, res) {
-  db.Article
-    .findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  db.Note
-    .create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-//----working route to change saved db boolean to true---//
-app.post("/saved/:id", function(req, res) {
-  console.log("save route hit");
-	db.Article.findOneAndUpdate({"_id": req.params.id}, {"saved": true}, {new:true})
-	.then(function(error, saved) {
-		if (error) {
-			console.log(error);
-		} else {
-			res.send(saved);
-		}
-	});
-});
+app.use(require('./controllers/routes'));
 
 
-app.post("/delete/:id", function(req, res) {
-  console.log("delete route hit");
-	db.Article.findOneAndUpdate({"_id": req.params.id}, {"saved":false}, {new:true})
-	.then(function(err, deleted) {
-    res.redirect("/saved");
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(deleted);
-		}
-	});
-});
-
-module.exports = app;
-
-
-///======connection test=======///
+///======  mongoose connection  config =======///
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_lslc7jm7:dv8fjvle7ad0floc6io1d2ks36@ds127105.mlab.com:27105/heroku_lslc7jm7";
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true
